529bd7519940ba97dec10d2c03fea64907579cd2
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-08 05:03:59 +0300

    Update iv.sh

--- lab2/iv.sh
+++ lab2/iv.sh
@@ -3,9 +3,9 @@
 for pid in $(ps -Ao pid | tail -n +2);
 do
 	pth="/proc/"$pid
-	ppid=$(grep -Ehsi "ppid:\s+(.+)" $pth"/status" | grep -o "[0-9]\+")
-	rt=$(grep -Ehsi "se\.sum_exec_runtime(.+):\s+(.+)" $pth/sched | awk '{print $3}')
-	sw=$(grep -Ehsi "nr_switches(.+):\s+(.+)" $pth/sched | awk '{print $3}')
+	ppid=$(grep -hsi "ppid:*" $pth"/status" | grep -o "[0-9]\+")
+	rt=$(grep -hsi "se\.sum_exec_runtime" $pth/sched | awk '{print $3}')
+	sw=$(grep -hsi "nr_switches" $pth/sched | awk '{print $3}')
 	if [ -z $ppid ];
 	then
 		ppid=0
@@ -14,7 +14,7 @@ do
 	then
 		art=0
 	else
-		art=$(echo "scale=2; $rt/$sw" | bc | awk '{printf "%.2f", $0}')
+		art=$(echo "scale=5; $rt/$sw" | bc)
 	fi
 	echo "$pid $ppid $art"
 done | sort -nk2 | awk '{print "ProcessID="$1" : Parent_ProcessID="$2" : Average_Running_Time="$3}' > iv.txt
