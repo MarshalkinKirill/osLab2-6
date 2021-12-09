b12447771c6f35986e5e1059896630792ab2b790
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-09 23:30:19 +0300

    Update 06.sh

--- lab_3/06.sh
+++ lab_3/06.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-while true;
-do
-	read line
-	case "$line" in
-		"TERM")
-			kill -TERM $1
-			exit 0
-		;;
-		"+")
-			kill -USR1 $1
-		;;
-		"*")
-			kill -USR2 $1
-		;;
-		*)
-			continue
-		;;
-	esac
-done
+./handler2.sh&pid=$!
+./generator2.sh $pid
