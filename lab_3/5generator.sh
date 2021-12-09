5fd071936a0fa05f9cb88ac76a1f479ee04c3899
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-09 23:29:06 +0300

    Update and rename 05.helper to 5generator.sh

---
+++ lab_3/5generator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true;
+do
+	read line
+	echo "$line" > pipe
+
+	if [[ "$line" == "QUIT" ]];
+	then
+		echo "quit"
+		exit 0
+	fi
+
+	if [[ "$line" != "+" && "$line" != "*" && "$line" != [0-9] ]];
+	then
+		echo "generator error"
+		exit 1
+	fi
+done
