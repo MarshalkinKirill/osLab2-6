ba6b3b515fdc2d678f3af546a9effd20f547bccc
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-09 22:42:55 +0300

    Update 04.sh

--- lab_3/04.sh
+++ lab_3/04.sh
@@ -7,5 +7,13 @@ pid2=$!
 ./04helper.sh &
 pid3=$!
 
+top -b -n 1 | head -10 | tail -4 | awk '{print $1 " " $9}'
+
 renice +10 -p $pid1
+
 kill $pid3
+
+top -b -n 1 | head -10 | tail -4 | awk '{print $1 " " $9}'
+
+kill $pid1
+kill $pid2
