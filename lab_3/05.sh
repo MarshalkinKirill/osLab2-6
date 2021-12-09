0354444a07afe039a0e593483c5366916548d8a2
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-09 23:24:03 +0300

    Update 05.sh

--- lab_3/05.sh
+++ lab_3/05.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-while true;
-do
-	read line
-	echo "$line" > pipe5
-
-	if [[ "$line" == "QUIT" ]];
-	then
-		exit 0
-	fi
-
-	if [[ "$line" != "+" && "$line" != "*" && "$line" != [0-9]* ]];
-	then 
-		echo "Wrong line"
-		exit 1
-	fi
-done
+mkfifo pipe
+./5handler.sh&./5generator.sh
+rm pipe
