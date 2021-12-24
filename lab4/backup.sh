51529a7d6697622b31a5e044f43a6da453c21e15
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-24 10:11:51 +0300

    Update backup.sh

--- lab4/backup.sh
+++ lab4/backup.sh
@@ -1,56 +1,53 @@
 #!/bin/bash
 
-home_path="/home/PC"
+home="/home/user"
+report="$home/.backup-report"
 
-if [[ ! -d "$home_path/source" ]]
+todayDate=$(date "+%Y-%m-%d")
+todayTime=$(date -d "$todayDate" "+%s")
+
+lastBackup=$(ls $home | grep -E "^Backup-" | sort -n | tail -1)
+lastBackupDate=$(echo $lastBackup | grep -E -o "[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+lastBackupTime=$(date -d "$lastBackupDate" +"%s")
+
+timeDifference=$(echo "($todayTime - $lastBackupTime) / 60 / 60 / 24" | bc)
+
+if [ "$timeDifference" -gt 7 ] || [[ -z "$lastBackup" ]]
 then
-	echo "Source directory doesn't exist"
-	exit 1
-fi
-
-backup_prev=$(ls "$home_path" | grep -E "^Backup-" | sort -n | tail -1)
-date_time_now=$(date +"%Y-%m-%d")
-date_time_now_sec=$(date -d "$date_time_now" +"%s")
-date_time_backup_prev=$(echo "$backup_prev" | sed "s/Backup-//" )
-date_time_backup_prev_sec=$(date -d "$date_time_backup_prev" +"%s")
-time_difference=$(echo "(${date_time_now_sec} - ${date_time_backup_prev_sec}) / (3600 * 24) " | bc)
-
-if [[ ! -e "$home_path/backup-report" ]]
-then
-	touch "$home_path/backup-report"
-fi
-
-
-if [[ "$time_difference" -ge 7 ]] || [[ "$backup_prev" == "" ]]
-then
-	mkdir "$home_path/Backup-${date_time_now}"
-	for line in $(ls "$home_path/source")
-	do
-		cp "$home_path/source/$line" "$home_path/Backup-${date_time_now}"
-	done
-	echo -e "[New backup]\tDate ${date_time_now}\n\t\tFiles: $(ls "$home_path/Backup-${date_time_now}" | tr '\n' ' ')\n" >> "$home_path/backup-report"
-
+	mkdir "$home/Backup-${todayDate}"
+	echo "Created directory $home/Backup-${todayDate}" >> $report
+	find "$home/source" -type f -print0 | while read -d $'\0' filePath
+    do
+		file=$(echo "$filePath" | awk 'BEGIN{FS="/"}; {print $NF}')
+	    cp "$home/source/$file" "$home/Backup-${todayDate}"
+		echo "Copied $file" >> $report
+    done
+	
 else
-	> changed_files_info.log
-	> new_files_info.log
-	for line in $(ls "$home_path/source")
+	changes=""
+	find "$home/source" -type f -print0 | while read -d $'\0' filePath
 	do
-		if [[ ! -f "$home_path/$backup_prev/$line" ]]
+		file=$(echo "$filePath" | awk 'BEGIN{FS="/"}; {print $NF}')
+		if [[ ! -f "$home/$lastBackup/$file" ]]
 		then
-			cp "$home_path/source/$line" "$home_path/$backup_prev"
-			echo "Copied: $line" >> new_files_info.log
+			cp "$home/source/$file" "$home/$lastBackup"
+			changes="$changes\n$file was added"
 		else
-			new_size=$(wc -c "$home_path/source/$line" | awk '{print $1}')
-			old_size=$(wc -c "$home_path/$backup_prev/$line" | awk '{print $1}')
-			if [[ "$new_size" -ne "$old_size" ]]
+			sourceSize=$(wc -c "$home/source/$file" | awk '{print $1}')
+			backupSize=$(wc -c "$home/$lastBackup/$file" | awk '{print $1}')
+			sizeDifference=$(echo "$sourceSize - $backupSize" | bc)
+			if [[ $sizeDifference != 0 ]]
 			then
-				mv "$home_path/$backup_prev/$line" "$home_path/$backup_prev/$line-${date_time_now}"
-				echo "[Changed] From: $line To: $line-${date_time_now}" >> changed_files_info.log
-				cp "$home_path/source/$line" "$home_path/$backup_prev/$line"
+				mv "$home/$lastBackup/$file" "$home/$lastBackup/$file.${todayDate}"
+				cp "$home/source/$file" "$home/$lastBackup"
+				changes="$changes\n$file is new version of $file.${todayDate}"
 			fi
 		fi
 	done
-	echo -e "[Change backup]\n\tDate ${date_time_backup_prev}\n\tNew files:\n$(cat new_files_info.log)\n\tChanged file:\n$(cat changed_files_info.log)" >> "$home_path/backup-report"
-	rm changed_files_info.log 2>/dev/null
-	rm new_files_info.log 2>/dev/null
+fi
+
+changes=$(echo $changes | sed 's/^\\n//')
+if [[ ! -z "$changes" ]]
+then
+	echo -e "$lastBackupDate Updated:\n${changes}" >> $report
 fi
