60e1dd5a5d3bc1880e23d2b8ff1f031f3fa5d5dc
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-24 06:48:12 +0300

    Update iv.sh

--- lab2/iv.sh
+++ lab2/iv.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-for pid in $(ps -Ao pid | tail -n +2);
+for pid in $(ps -Ao pid | tail -n+2)
 do
-	pth="/proc/"$pid
-	ppid=$(grep -hsi "ppid:*" $pth"/status" | grep -o "[0-9]\+")
-	rt=$(grep -hsi "se\.sum_exec_runtime" $pth/sched | awk '{print $3}')
-	sw=$(grep -hsi "nr_switches" $pth/sched | awk '{print $3}')
-	if [ -z $ppid ];
-	then
-		ppid=0
-	fi
-	if [ -z $rt ] || [ -z $sw ];
-	then
-		art=0
-	else
-		art=$(echo "scale=5; $rt/$sw" | bc)
-	fi
-	echo "$pid $ppid $art"
-done | sort -nk2 | awk '{print "ProcessID="$1" : Parent_ProcessID="$2" : Average_Running_Time="$3}' > iv.txt
+    ppid=$(grep -s "PPid:" /proc/"$pid"/status | grep -o "[[:digit:]]\+")
+    runtime=$(grep -s "se\.sum_exec_runtime" /proc/"$pid"/sched | grep -o "[[:digit:]]\+\.[[:digit:]]\+")
+    switches=$(grep -s "nr_switches" /proc/"$pid"/sched | grep -o "[[:digit:]]\+")
+    if [ -z "$runtime" ]
+        then continue
+    fi
+    echo "$pid" "$ppid" $(echo "scale=5; $runtime/$switches" | bc)
+done | sort -nk 2 | awk '{print "ProcessID="$1" : Parent_ProcessID="$2" : Average_Running_Time="$3""}' > iv.txt
