78ac0983997cee3f4bd13c183104daec2cf5e317
MarshalkinKirill <72259944+MarshalkinKirill@users.noreply.github.com>
2021-12-24 06:51:22 +0300

    Update vi.sh

--- lab2/vi.sh
+++ lab2/vi.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
-grep "VmSize" /proc/*/status | sed "s/[^0-9.]\+/ /g" | sort -nk2 | tail -1 | awk '{print " Pid : "$1" VmSize : "$2}'
-
-top -b -n1 -o RES | head -n 8 | tail -n 1 | awk '{print " Pid: " $1 " Virt: " $6}'
+max=0
+maxpid=0
+for pid in $(ps -Ao pid)
+do
+    if [[ -r /proc/"$pid"/status ]]
+    then
+	res=$(grep -s "VmRSS:" /proc/"$pid"/status | grep -o "[[:digit:]]\+")
+	if [ "$res" != "" ] && [ "$res" -gt "$max" ]
+	then
+	    max=$res
+	    maxpid=$pid
+	fi
+    fi
+done
+echo "[by /proc] PID = "$maxpid" : Uses Memory = "$max""
+top -bo %MEM -n 1 | tail -n+8 | head -1 | awk '{print "[by top] PID = "$1" : Uses Memory = "$6""}'
